9551f048a09879f6d794676375b290d3002ff228
patrickclery <patrick.clery@gmail.com>
2020-02-05 21:26:26 -0800

    :white_check_mark: Adding tests.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,20 @@
 class Flight < ApplicationRecord
   belongs_to :rocket, foreign_key: :rocket_id, required: true
 
+  # These scopes are meant to be used to filter results for the API
+  scope :with_reddit_links, -> {
+    # Rails uses the WRONG operator (!=) with JSONB in PostgreSQL, and therefore raw SQL is necessary
+    conditions = <<SQL
+      links->>'reddit_campaign' <> '' OR
+      links->>'reddit_launch' <> '' OR
+      links->>'reddit_recovery' <> '' OR
+      links->>'reddit_media' <> ''
+SQL
+    where(conditions)
+  }
+  scope :with_successful_launches, -> { where(launch_successful: true) }
+  scope :with_reuses, -> { where(is_reused: true) }
+
   # Type-cast all the values to make validation easier
   attribute :details, :string
   attribute :is_reused, :boolean
--- test/factories/flight_factory.rb
+++ test/factories/flight_factory.rb
@@ -7,26 +7,31 @@ FactoryBot.define do
     local_launched_at { Faker::Time.between_dates(from: 5.years.ago, to: 3.years.ago) }
 
     links {
-      links_hash = {
+      {
         "mission_patch":       Faker::Internet.url,
         "mission_patch_small": Faker::Internet.url,
-        "article_link":        Faker::Internet.url
+        "article_link":        Faker::Internet.url,
+        "reddit_campaign":     '',
+        "reddit_launch":       '',
+        "reddit_recovery":     '',
+        "reddit_media":        ''
       }
-      # Randomly add reddit or not. Using real links
-      if Faker::Boolean.boolean
-        links_hash.merge!(
-          {
-            "reddit_campaign": Faker::Internet.url,
-            "reddit_launch":   Faker::Internet.url,
-            "reddit_recovery": Faker::Internet.url,
-            "reddit_media":    Faker::Internet.url
-          }
-        )
-      end
-
-      links_hash
     }
     rocket
 
+    trait :with_reddit_links do
+      # Randomly add reddit or not. Using real links
+      links {
+        {
+          "mission_patch":       Faker::Internet.url,
+          "mission_patch_small": Faker::Internet.url,
+          "article_link":        Faker::Internet.url,
+          "reddit_campaign":     Faker::Internet.url,
+          "reddit_launch":       Faker::Internet.url,
+          "reddit_recovery":     Faker::Internet.url,
+          "reddit_media":        Faker::Internet.url
+        }
+      }
+    end
   end
 end
