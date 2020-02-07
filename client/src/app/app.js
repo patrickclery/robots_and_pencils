5c5386073e8d794afe99a5594547606abc5684f9
patrickclery <patrick.clery@gmail.com>
2020-02-06 17:31:29 -0800

    :tada: Initial commit of React frontend

---
+++ client/src/app/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../scss/styles.css'
+
+class App extends Component {
+	render() {
+		return (
+			<div id="app" className="page-wrapper">
+				<h1>SpaceX Launches</h1>
+			</div>
+
+			<div class="rectangle">
+		)
+	}
+}
+
+export default App
