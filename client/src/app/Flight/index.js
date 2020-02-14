4bad1b5e89a2979127b5ea79b673cdef821ba20e
patrickclery <patrick.clery@gmail.com>
2020-02-13 21:55:40 -0800

    :sparkles: Introducing new features. Front-end has a basic scaffold but will do refactoring on CSS names and backend properties NOTE: CORS required to accept requests from localhost

---
+++ client/src/app/Flight/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './index.css'
+import LinkIcon from '../LinkIcon'
+
+function Flight({data: {attributes, links}}) {
+
+  const {rocketName, rocketType, localLaunchedAt, details, referenceNumber} = attributes;
+  const {articleLink, redditCampaign} = links;
+
+  return (
+    <div className="flight__grid-row">
+      <div className="flight__grid-cell flight__grid-cell--badge"></div>
+      <div className="flight__grid-cell">{rocketName}</div>
+      <div className="flight__grid-cell">{rocketType}</div>
+      <div className="flight__grid-cell">2020-02-02</div>
+      <div className="flight__grid-cell">{details}</div>
+      <div className="flight__grid-cell flight__grid-cell--referenceNumber">{referenceNumber}</div>
+      <div className="flight__grid-cell">
+        {articleLink && <a href={articleLink}>
+          <LinkIcon color="#00f"/>
+        </a>}
+        <a href="test">
+          <LinkIcon color="#00f"/>
+        </a>
+        {redditCampaign && <a href={redditCampaign}>
+          <LinkIcon color="#00f"/>
+        </a>}
+      </div>
+    </div>
+  )
+}
+
+export default Flight
--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,15 +1,50 @@
-import React, { Component } from 'react'
-import '../scss/styles.css'
+import React, {Component} from 'react';
+import Flight from './Flight';
+import '../scss/styles.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div id="app" className="page-wrapper">
-				<h1>SpaceX Launches</h1>
-			</div>
+  state = {flights: []};
 
-		)
-	}
+  componentDidMount() {
+    this.refreshData();
+  }
+
+  refreshData() {
+    fetch("http://localhost:3000/flights")
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({flights: myJson.data});
+      });
+  }
+
+  render() {
+    const {flights} = this.state;
+    return (
+      <div className="app--space-theme">
+        <div className="heading--h1">
+          SpaceX Launches
+        </div>
+        <div className="flights__grid">
+          <div className="flights__headers">
+            <div className="flights__headers__th">Badges</div>
+            <div className="flights__headers__th">Rocket Name</div>
+            <div className="flights__headers__th">Rocket Type</div>
+            <div className="flights__headers__th">Launch Date</div>
+            <div className="flights__headers__th">Details</div>
+            <div className="flights__headers__th">ID</div>
+            <div className="flights__headers__th">Article</div>
+          </div>
+          {flights.map((flight) =>
+                         <Flight data={flight}
+                                 key={flight.id}/>
+          )}
+          <div className="flights__line">&nbsp;</div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
