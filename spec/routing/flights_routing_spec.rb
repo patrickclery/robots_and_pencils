fc7cb68904706667d05197764af182130ca87142
Patrick Clery <patrick.clery@gmail.com>
2020-02-25 11:53:38 -0800

    :pencil2: Fixing test bug (#17)
    A variable rename (with_reddit_links -> with_reddit) and adding an API namespacing (/api/v1/) caused 8 tests to fail. Fixed

--- spec/routing/flights_routing_spec.rb
+++ spec/routing/flights_routing_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Api::V1::FlightsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/flights").to route_to("flights#index")
+      expect(:get => "/api/v1/flights").to route_to("api/v1/flights#index")
     end
   end
 end
