50c5dede358681b4af70cc053255a96c948d30aa
patrickclery <patrick.clery@gmail.com>
2020-02-25 19:40:55 -0800

    :truck: Moving or renaming files for react-rails

--- frontend/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders SpaceX hweadline', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/SpaceX Launches/i);
-  expect(linkElement).toBeInTheDocument();
-});
