2ce77f71ad9dd32cd05a18a52e7a41559192e60a
patrickclery <patrick.clery@gmail.com>
2020-03-08 01:54:05 -0800

    :see_no_evil: Adding or updating a .gitignore file. (add public/packs)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
 /* eslint no-console:0 */
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
