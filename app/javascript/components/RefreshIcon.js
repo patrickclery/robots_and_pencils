e68dba33c26c1dcd6151208c7388a5bcc38eac7c
patrickclery <patrick.clery@gmail.com>
2020-03-09 23:45:07 -0700

    :bug: Fixing a bug. was getting warnings for using kebab-case not camelCase for props

--- app/javascript/components/RefreshIcon.js
+++ app/javascript/components/RefreshIcon.js
@@ -4,12 +4,12 @@ function RefreshIcon(props) {
   return (
     <svg width="73" height="74" viewBox="0 0 73 74" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g filter="url(#filter0_d)">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M36.3613 52.4774C45.9496 52.4774 53.7225 44.5355 53.7225 34.7387C53.7225 24.9419 45.9496 17 36.3613 17C26.7729 17 19 24.9419 19 34.7387C19 44.5355 26.7729 52.4774 36.3613 52.4774Z" stroke="white" stroke-width="2"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M36.3613 52.4774C45.9496 52.4774 53.7225 44.5355 53.7225 34.7387C53.7225 24.9419 45.9496 17 36.3613 17C26.7729 17 19 24.9419 19 34.7387C19 44.5355 26.7729 52.4774 36.3613 52.4774Z" stroke="white" strokeWidth="2"/>
       </g>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M36.364 24.6765V27.3088C40.1727 27.3088 43.2575 30.4501 43.2575 34.3284C43.2575 35.706 42.8611 36.9827 42.1933 38.0707L40.9352 36.7897C41.3144 36.0526 41.5341 35.219 41.5341 34.3284C41.5341 31.4197 39.2205 29.0637 36.364 29.0637V31.6961L32.9173 28.1863L36.364 24.6765ZM31.1939 34.3284C31.1939 37.2372 33.5076 39.5931 36.364 39.5931V36.9608L39.8108 40.4705L36.364 43.9803V41.348C32.5554 41.348 29.4706 38.2067 29.4706 34.3284C29.4706 32.9508 29.867 31.6741 30.5348 30.5861L31.7928 31.8672C31.4137 32.6042 31.1939 33.4378 31.1939 34.3284Z" fill="white"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M36.364 24.6765V27.3088C40.1727 27.3088 43.2575 30.4501 43.2575 34.3284C43.2575 35.706 42.8611 36.9827 42.1933 38.0707L40.9352 36.7897C41.3144 36.0526 41.5341 35.219 41.5341 34.3284C41.5341 31.4197 39.2205 29.0637 36.364 29.0637V31.6961L32.9173 28.1863L36.364 24.6765ZM31.1939 34.3284C31.1939 37.2372 33.5076 39.5931 36.364 39.5931V36.9608L39.8108 40.4705L36.364 43.9803V41.348C32.5554 41.348 29.4706 38.2067 29.4706 34.3284C29.4706 32.9508 29.867 31.6741 30.5348 30.5861L31.7928 31.8672C31.4137 32.6042 31.1939 33.4378 31.1939 34.3284Z" fill="white"/>
       <defs>
-        <filter id="filter0_d" x="0" y="0" width="72.7225" height="73.4774" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter0_d" x="0" y="0" width="72.7225" height="73.4774" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
           <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
           <feOffset dy="2"/>
           <feGaussianBlur stdDeviation="9"/>
