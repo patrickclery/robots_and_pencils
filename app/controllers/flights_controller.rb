11ba3460e0cba1ae791e1e737d2c5f6fe2af3ab0
patrickclery <patrick.clery@gmail.com>
2020-03-09 23:31:30 -0700

    :recycle: Refactoring code. Converted App.js from functional component to class component to see if this fixes the Babel issues

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ActionController::Base
   def index
     @flights = Flight.all
-    render component: 'App', layout: 'application', prerender: false
+    render :index, layout: 'application'
   end
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,84 +1,88 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Flight from './Flight';
 import Header from './Header';
 import './../src/App.css';
 import RefreshIcon from './RefreshIcon';
 
-function App() {
+class App extends React.Component {
 
-  const [flights, setFlights] = useState([]);
-  const [filters, setFilters] = useState({
-                                           withRedditLinks:        0,
-                                           withSuccessfulLaunches: 0,
-                                           withReuses:             0
-                                         });
+  state = {
+    flights: [],
+    filters: {
+      withRedditLinks:        0,
+      withSuccessfulLaunches: 0,
+      withReuses:             0
+    }
+  };
 
   // This takes the filters as params, then returns a JSON of the flights
-  const refreshFlights = () => {
+  refreshFlights = () => {
     let url = "http://localhost:3000/api/v1/flights?";
-    url = `${url}with_reuses=${encodeURI(filters.withReuses)}&`;
-    url = `${url}with_reddit=${encodeURI(filters.withRedditLinks)}&`;
-    url = `${url}with_successful_launches=${encodeURI(filters.withSuccessfulLaunches)}`;
+    url = `${url}with_reuses=${encodeURI(this.state.filters.withReuses)}&`;
+    url = `${url}with_reddit=${encodeURI(this.state.filters.withRedditLinks)}&`;
+    url = `${url}with_successful_launches=${encodeURI(this.state.filters.withSuccessfulLaunches)}`;
 
     fetch(url)
       .then(response => {
         return response.json();
       })
       .then(myJson => {
-        setFlights(myJson.data);
+        this.setState({flights: myJson.data});
       });
   };
 
 
-  const handleChange = (event) => {
+  handleChange = event => {
     let result = {[event.target.name]: (event.target.checked ? 1 : 0)};
-    setFilters({...filters, ...result});
+    this.setState({filters: {...this.filters, ...result}});
   };
 
-  useEffect(() => {
-    refreshFlights(filters)
-  }, [filters]);
+  componentDidMount() {
+    this.refreshFlights();
+  }
 
-  return (
-    <div className="app--space-x">
-      <div className="container">
-        <div className="headline">
-          SpaceX Launches
-        </div>
-        <div className="toolbar">
-          <div className="toolbar-col--refresh">
-            <RefreshIcon/>
+  render() {
+    return (
+      <div className="app--space-x">
+        <div className="container">
+          <div className="headline">
+            SpaceX Launches
           </div>
-          <div className="toolbar-col--filter">
-            <label id="withSuccessfulLaunches">
-              <input type="checkbox" name="withSuccessfulLaunches" defaultChecked={false}
-                     onChange={handleChange}/> Land Success
-            </label>
-          </div>
+          <div className="toolbar">
+            <div className="toolbar-col--refresh">
+              <RefreshIcon/>
+            </div>
+            <div className="toolbar-col--filter">
+              <label id="withSuccessfulLaunches">
+                <input type="checkbox" name="withSuccessfulLaunches" defaultChecked={false}
+                       onChange={this.handleChange}/> Land Success
+              </label>
+            </div>
 
-          <div className="toolbar-col--filter">
-            <label id="withReuses">
-              <input type="checkbox" name="withReuses" defaultChecked={false}
-                     onChange={handleChange}/> Reused
-            </label>
+            <div className="toolbar-col--filter">
+              <label id="withReuses">
+                <input type="checkbox" name="withReuses" defaultChecked={false}
+                       onChange={this.handleChange}/> Reused
+              </label>
+            </div>
+            <div className="toolbar-col--filter">
+              <label id="withRedditLinks">
+                <input type="checkbox" name="withRedditLinks" defaultChecked={false}
+                       onChange={this.handleChange}/> With Reddit
+              </label>
+            </div>
           </div>
-          <div className="toolbar-col--filter">
-            <label id="withRedditLinks">
-            <input type="checkbox" name="withRedditLinks" defaultChecked={false}
-                   onChange={handleChange}/> With Reddit
-            </label>
+          <div className="flights">
+            <Header/>
+            {this.state.flights.map((flight) =>
+                           <Flight data={flight}
+                                   key={flight.id}/>
+            )}
           </div>
-        </div>
-        <div className="flights">
-          <Header/>
-          {flights.map((flight) =>
-                         <Flight data={flight}
-                                 key={flight.id}/>
-          )}
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
