7c1a6e9afe70e624aef7f8eb0a2e7ed7b1f7371c
patrickclery <patrick.clery@gmail.com>
2020-04-05 03:04:27 -0700

    :sparkles: Introducing new features. Merged all enhancements and configuration changes from dry-rails-react project: https://github.com/patrickclery/dry-rails-react

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,7 @@
-require 'factory_bot_rails'
-require 'factory_bot'
+# frozen_string_literal: true
+
+require "factory_bot_rails"
+require "factory_bot"
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ### Support for CodeCov
 SimpleCov.start
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+SimpleCov.formatter = SimpleCov::Formatter::Coveralls
